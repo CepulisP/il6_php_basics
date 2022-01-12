7a7a6e603a72351bd8522fefba7a4ae5d56352ed
CepulisP <cepulisp@gmail.com>
2022-01-12 23:11:11 +0200

    lesson 9

--- subscribe/index.php
+++ subscribe/index.php
@@ -1,32 +1,25 @@
 <!DOCTYPE html>
 <html>
-<head><title>Our website</title></head>
-<body>
-<h2>Subscribe</h2>
-<form action="http://localhost/pamokos/subscribe/index.php" method="post">
-    <input type="email" name="email" placeholder="name@mail.com">
-    <input type="submit" value="Subscribe">
-</form>
-</body>
-</html>
-
-<?php
-
-include 'helper.php';
-
-if (isset($_POST['email'])) {
-    $email = cleanString($_POST['email']);
-    if (isEmailValid($email)) {
-        if (isValueUniq($email, 'emails.csv')) {
-            writeToCsv($email, 'emails.csv');
-            echo '<br>';
-            echo '<b>Thank you for subscribing</b>';
-        } else {
-            echo '<br>';
-            echo 'Email taken';
+    <head>
+        <title>Our website</title>
+    </head>
+    <body>
+        <h2>Subscribe</h2>
+        <form action="http://localhost/pamokos/subscribe/index.php" method="post">
+            <input type="email" name="email" placeholder="name@mail.com">
+            <input type="submit" value="Subscribe">
+        </form>
+        <?php
+        include 'helper.php';
+        if (isset($_POST['email'])) {
+            $email = cleanString($_POST['email']);
+            if (isEmailValid($email) && isValueUniq($email, 'emails.csv')) {
+                    writeToCsv($email, 'emails.csv');
+                    echo '<br><b>Thank you for subscribing</b>';
+                } else {
+                    echo '<br>Email is invalid';
+            }
         }
-    } else {
-        echo '<br>';
-        echo 'Check your email';
-    }
-}
+        ?>
+    </body>
+</html>
