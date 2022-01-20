62c6f903bf6015a3717a9c51066cc6b6905302ec
CepulisP <cepulisp@gmail.com>
2022-01-20 22:57:17 +0200

    compelted a random task

--- first_lessons/random_stuff.php
+++ first_lessons/random_stuff.php
@@ -1,8 +1,20 @@
-<?php
-
-for ($i = 1; $i < 32; $i++) {
-    echo $i . ' ';
-    if ($i % 7 == 0) {
-        echo '<br>';
-    }
-}
+<!DOCTYPE html>
+    <html>
+    <head>
+        <title>Random stuff</title>
+    </head>
+    <body>
+        <table>
+            <tr>
+            <?php
+            for ($i = 1; $i < 32; $i++) {
+                echo '<td>'.$i . '</td>';
+                if ($i % 7 == 0) {
+                    echo '</tr><tr>';
+                }
+            }
+            ?>
+            </tr>
+        </table>
+    </body>
+</html>
