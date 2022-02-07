050088708001925e01c25dfcb35ad2f73744c702
CepulisP <cepulisp@gmail.com>
2022-02-07 21:48:51 +0200

    more work on home page

--- oop/app/design/parts/home.php
+++ oop/app/design/parts/home.php
@@ -1,2 +1,6 @@
-<h2 style="text-align:center;">Home page</h2>
-<pre><?php print_r($_SESSION); ?></pre>
+<div>
+    <h2 style="text-align:center;">Home page</h2>
+    <pre>
+        <?php print_r($_SESSION); ?>
+    </pre>
+</div>
