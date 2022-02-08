0bf5a52079ea32c11ecfbca91fbd1588808484a6
CepulisP <cepulisp@gmail.com>
2022-02-08 19:05:12 +0200

    formated nav

--- oop/app/design/parts/header.php
+++ oop/app/design/parts/header.php
@@ -6,15 +6,13 @@
 <header style="text-align:center;">
     <h1 style="color:white;">Gumtree</h1>
     <nav>
-        <ul style="list-style-type: none">
-            <li><a href='<?php echo BASE_URL ?>' style="color:white;text-decoration:none">&bullet; Home</a></li>
-            <li><a href='<?php echo BASE_URL ?>catalog/all/' style="color:white;text-decoration:none">&bullet; All ads</a></li>
-            <li><a href='<?php echo BASE_URL ?>catalog/add/' style="color:white;text-decoration:none">&bullet; New ad</a></li>
-            <li><a href='<?php echo BASE_URL ?>user/register/' style="color:white;text-decoration:none">&bullet; Sign up</a></li>
-            <li><a href='<?php echo BASE_URL ?>user/login/' style="color:white;text-decoration:none">&bullet; Login</a></li>
-            <li><a href='<?php echo BASE_URL ?>user/edit/' style="color:white;text-decoration:none">&bullet; Edit user</a></li>
-            <li><a href='<?php echo BASE_URL ?>user/logout/' style="color:white;text-decoration:none">&bullet; Logout</a></li>
-        </ul>
+        <a href='<?php echo BASE_URL ?>' style="color:white;text-decoration:none">&bullet; Home</a>
+        <a href='<?php echo BASE_URL ?>catalog/all/' style="color:white;text-decoration:none">&bullet; All ads</a>
+        <a href='<?php echo BASE_URL ?>catalog/add/' style="color:white;text-decoration:none">&bullet; New ad</a>
+        <a href='<?php echo BASE_URL ?>user/register/' style="color:white;text-decoration:none">&bullet; Sign up</a>
+        <a href='<?php echo BASE_URL ?>user/login/' style="color:white;text-decoration:none">&bullet; Login</a>
+        <a href='<?php echo BASE_URL ?>user/edit/' style="color:white;text-decoration:none">&bullet; Edit user</a>
+        <a href='<?php echo BASE_URL ?>user/logout/' style="color:white;text-decoration:none">&bullet; Logout</a>
     </nav>
     <hr>
 </header>
