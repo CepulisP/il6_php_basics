41cf563cbb82f18972cce7ad71e286955c60d7c5
CepulisP <cepulisp@gmail.com>
2022-03-12 19:46:16 +0200

    styled 404 page

--- oop/app/design/parts/errors/error404.php
+++ oop/app/design/parts/errors/error404.php
@@ -1,3 +1,9 @@
-<h2>
-    404 - Page not found
-</h2>
+<h1 class="glitch">
+    <span aria-hidden="true">404 not found</span>
+    404 not found
+    <span aria-hidden="true">404 not found</span>
+</h1>
+<div class="page-nf">
+    <p class="page-nf-msg">The page you're looking for does not exist.</p>
+    <a href='<?= $this->link('') ?>' class="btn">Home page</a>
+</div>
