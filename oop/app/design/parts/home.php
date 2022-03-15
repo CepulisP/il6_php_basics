cc2dddb6db39c8beeb9387f23023425bffbe0a23
CepulisP <cepulisp@gmail.com>
2022-03-15 22:50:26 +0200

    started styling home page

--- oop/app/design/parts/home.php
+++ oop/app/design/parts/home.php
@@ -1,61 +1,34 @@
-<div>
-    <div class="home-title">
-        <h2>Home page</h2>
-    </div>
-    <div class="greeting">
-        <?php if (isset($_SESSION['user_id'])) : ?>
-            <h3>Hello, <?= ucfirst($_SESSION['user']->getName()) ?>!</h3>
-            <br>
-        <?php endif ?>
-    </div>
-    <div class="home-content">
-        <div class="new-ads">
-            <div class="new-ads-title">
-                <h3>Newest ads</h3>
-            </div>
-            <div class="new-ads-content">
-                <table>
-                    <tr>
-                        <?php foreach ($this->data['new_ads'] as $ad) : ?>
-                            <td>
-                                <a href="<?= $this->link('catalog/show', $ad->getSlug()) ?>">
-                                    <b>
-                                        <?= ucfirst($ad->getTitle()) ?>
-                                    </b>
-                                    <br>
-                                    <img src="<?= $ad->getImage() ?>">
-                                    <br>
-                                    <?= $ad->getPrice() ?> Eur
-                                </a>
-                            </td>
-                        <?php endforeach; ?>
-                    </tr>
-                </table>
-            </div>
+<div class="home-content">
+    <div class="new-ads">
+        <div class="new-ads-title">
+            <h3>Newest ads</h3>
         </div>
-        <div class="most-popular-ads">
-            <div class="most-popular-ads-title">
-                <h3>Most popular ads</h3>
-            </div>
-            <div class="most-popular-ads-content">
-                <table>
-                    <tr>
-                        <?php foreach ($this->data['pop_ads'] as $ad) : ?>
-                            <td>
-                                <a href="<?= $this->link('catalog/show', $ad->getSlug()) ?>">
-                                    <b>
-                                        <?= ucfirst($ad->getTitle()) ?>
-                                    </b>
-                                    <br>
-                                    <img src="<?= $ad->getImage() ?>">
-                                    <br>
-                                    <?= $ad->getPrice() ?> Eur
-                                </a>
-                            </td>
-                        <?php endforeach; ?>
-                    </tr>
-                </table>
-            </div>
+        <div class="new-ads-content item-row">
+            <?php foreach ($this->data['new_ads'] as $ad) : ?>
+                    <a href="<?= $this->link('catalog/show', $ad->getSlug()) ?>">
+                        <div class="ad-container">
+                            <?= ucfirst($ad->getTitle()) ?>
+                            <img src="<?= $ad->getImage() ?>">
+                            <?= $ad->getPrice() ?> Eur
+                        </div>
+                    </a>
+            <?php endforeach; ?>
+        </div>
+    </div>
+    <div class="pop-ads">
+        <div class="pop-ads-title">
+            <h3>Most popular ads</h3>
+        </div>
+        <div class="pop-ads-content item-row">
+                <?php foreach ($this->data['pop_ads'] as $ad) : ?>
+                    <a href="<?= $this->link('catalog/show', $ad->getSlug()) ?>">
+                        <div class="ad-container">
+                            <?= ucfirst($ad->getTitle()) ?>
+                            <img src="<?= $ad->getImage() ?>">
+                            <?= $ad->getPrice() ?> Eur
+                        </div>
+                    </a>
+                <?php endforeach; ?>
         </div>
     </div>
 </div>
