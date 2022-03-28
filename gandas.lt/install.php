b0a75646be3d0dc866dc6c228f502040b3b0a85f
CepulisP <cepulisp@gmail.com>
2022-03-28 22:26:49 +0300

    sql builder

--- gandas.lt/install.php
+++ gandas.lt/install.php
@@ -3,10 +3,10 @@
 include 'config.php';
 
 try {
-    $conn = new \PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME, DB_USER, DB_PASS);
+    $conn = new \PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME, DB_USER, DB_PASS);
     $conn->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
     //echo "Connected successfully";
-} catch(\PDOException $e) {
+} catch (\PDOException $e) {
     echo "Connection failed: " . $e->getMessage();
 }
 
